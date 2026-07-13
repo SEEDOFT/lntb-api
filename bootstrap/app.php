65488ac7aea7ff1dc79638da67e7235b95c73647
Koy Yotraboth <koyyotrabothffc001@gmail.com>
2026-07-13 23:53:50 +0700

    lntb API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(static function (Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*'),
-        );
-    })->create();
+    ->withExceptions(static function (Exceptions $exceptions): void {
+        $exceptions->shouldRenderJsonWhen(static fn (Request $request): bool => true);
+    })
+    ->create();
